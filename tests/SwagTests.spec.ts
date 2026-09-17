3eba007671520a429ba7968918f913633217fc10
MadBat-XT <cruz.rmpg@gmail.com>
2026-09-17 16:38:38 +0300

    Add tests

---
+++ tests/SwagTests.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test("e2e", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  await expect(page).toHaveTitle("Swag Labs");
+
+  // Ввод логина и пароля
+  await page.getByPlaceholder("Username").fill("standard_user");
+  await page.getByPlaceholder("Password").fill("secret_sauce");
+  await page.getByRole("button").click();
+
+  //Сортировка по убыванию цены
+  await page
+    .locator('[data-test="product-sort-container"]')
+    .selectOption({ label: "Price (high to low)" });
+
+  //Добавление флиосовой толстовки и портфеля в корзину
+  await page
+    .locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]')
+    .click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+  //Переход в корзину
+  await page.locator('[data-test="shopping-cart-link"]').click();
+});
